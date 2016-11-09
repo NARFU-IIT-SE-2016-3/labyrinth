190ee878511151f98826e14ba95ef7a4be80f669
qqoen <qqoenous@gmail.com>
2016-11-09 20:12:07 +0300

    Code cleanup

---
+++ Assets/Scripts/DialogLoader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Xml;
+using System.Xml.Serialization;
+
+public class DialogLoader : MonoBehaviour
+{
+	public void Start()
+    {
+        var dialog = ReadXml("sample.xml");
+	}
+
+    public XmlDocument ReadXml(string fileName)
+    {
+        var xml = new XmlDocument();
+        xml.Load("Assets/Dialogs/" + fileName);
+        return xml;
+    }
+}
